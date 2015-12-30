e109ba9d3c1a0b380dce86ac7f4ee36a41ed8a13
José Pinto <jose.vc.pinto@gmail.com>
2015-12-30 03:20:52 +0000

    Update Program.cs

--- EasyHosts CLI/Program.cs
+++ EasyHosts CLI/Program.cs
@@ -51,6 +51,7 @@ namespace EasyHosts_CLI
                 Console.Clear();
                 printHeader(false);
             }
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
         }
 
         private static void processAddCommand(string[] tokens)
